cfbeb4eb91b56299284cdf2b45d0c6da12e9d0b0
Dracea Lucian <dracea.lucian.mihai@gmail.com>
2023-07-03 22:06:55 +0300

    initial commit

---
+++ src/main/java/com/app/prosport/ProsportApplication.java
@@ -0,0 +1,12 @@
+package com.app.prosport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsportApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProsportApplication.class, args);
+	}
+
+}
