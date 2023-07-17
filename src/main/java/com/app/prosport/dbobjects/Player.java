275b9edbf11f822f04f8744b9e9b300c4163180b
Dracea Lucian <dracea.lucian.mihai@gmail.com>
2023-07-17 13:17:22 +0300

    added player validation

--- src/main/java/com/app/prosport/dbobjects/Player.java
+++ src/main/java/com/app/prosport/dbobjects/Player.java
@@ -3,6 +3,8 @@ package com.app.prosport.dbobjects;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,13 +27,19 @@ public class Player {
     @JsonIgnore
     private Team assignedTeam;
 
+    @NotEmpty(message = "First name required")
     private String firstName;
+    @NotEmpty(message = "Last name required")
     private String lastName;
+
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate registrationDate;
 
     //Years, Centimeters, Kilograms
+    @NotNull(message = "Age required")
     private Integer age;
+    @NotNull(message = "Weight required")
     private Integer weight;
+    @NotNull(message = "Height required")
     private Integer height;
 }
